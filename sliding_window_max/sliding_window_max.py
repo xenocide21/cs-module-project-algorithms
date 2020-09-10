376f217e2fe287107583d8985c738d09d54b864b
Ashley Dunham <xenocide21@gmail.com>
2020-09-10 10:45:43 -0700

    sliding_window_max.py complete

--- sliding_window_max/sliding_window_max.py
+++ sliding_window_max/sliding_window_max.py
@@ -2,10 +2,22 @@
 Input: a List of integers as well as an integer `k` representing the size of the sliding window
 Returns: a List of integers
 '''
+
+
 def sliding_window_max(nums, k):
     # Your code here
+    result_arr = []
 
-    pass
+    low = 0
+    high = k - 1
+    while high != len(nums):
+        window_values = nums[low:high + 1]  # here + 1 because of the split
+        window_max = max(window_values)
+        low += 1
+        high += 1
+        result_arr.append(window_max)
+
+    return result_arr
 
 
 if __name__ == '__main__':

9cab07443f40b98082c4b49a1686c17d569df541
Ashley Dunham <xenocide21@gmail.com>
2020-09-10 10:58:06 -0700

    eating_cookies.py complete, project mvp met

--- eating_cookies/eating_cookies.py
+++ eating_cookies/eating_cookies.py
@@ -2,10 +2,22 @@
 Input: an integer
 Returns: an integer
 '''
-def eating_cookies(n):
-    # Your code here
 
-    pass
+
+def eating_cookies(n, jar=None):
+    # Your code here
+    if n == 0:
+        return 1
+    elif n < 0:
+        return 0
+    elif jar and jar[n] > 0:
+        return jar[n]
+    else:
+        if not jar:
+            jar = [0 for _ in range(n + 1)]
+        jar[n] = eating_cookies(n - 1, jar) + eating_cookies(n - 2, jar) + eating_cookies(n - 3, jar)
+    return jar[n]
+
 
 if __name__ == "__main__":
     # Use the main function here to test out your implementation
